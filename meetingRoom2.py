0764e5cc689c006babe8cb2c28ea31354bebeff3
Anjali Tambe <60959990+AnjuTambe@users.noreply.github.com>
2025-10-07 13:44:09 -0700

    using min heap solve the problem of meeting room 2

---
+++ meetingRoom2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        # intervals[i] = [starti  , endi] = 
+        intervals.sort()
+        meeting_rooms = 1
+        heap = [intervals[0][1]]
+
+        for start , end in intervals[1 :] :
+
+            if heap[0] <= start : 
+                heapq.heappop(heap)
+            heapq.heappush(heap , end)
+            
+            meeting_rooms = max(meeting_rooms , len(heap))
+
+        return  meeting_rooms 
+
+        #TC : O(nlogn)
+        #SC : O(n) 
+            
