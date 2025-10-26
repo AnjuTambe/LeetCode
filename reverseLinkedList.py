fa01cf7afe1c1025a1e748b9538943d7238b1fd7
Anjali Tambe <60959990+AnjuTambe@users.noreply.github.com>
2025-10-26 08:50:35 -0700

    reverse the linked list

---
+++ reverseLinkedList.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        prev = None
+        curr = head
+        while curr : 
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev    
